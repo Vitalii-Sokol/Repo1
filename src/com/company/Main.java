443d29b63521a632ef41a5c32d76f01fdb5a05c9
Vitalii-Sokol <vitaliy.sokol91@gmail.com>
2015-03-25 19:58:18 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,61 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        /*int x = 10;
+        int y = 20;
+        double d = 25.5;
+        String f = "Hello";
+        //System.out.println(12+20+f);
+        *///String sum = "Sum of 256 and 3150";
+
+       /*
+        int a = (256 + 3150);
+        System.out.print("Sum of 256 and 3150:" + a);
+        double e = 180.0/31.0;
+        System.out.print("180/31" + e);
+        */
+
+        /*String h = "Some \n 10\n  string";
+        System.out.print(h);
+        */
+        boolean t = true;
+        boolean f = false;
+
+        t = 5 == 5;
+        f = 0 == 10;
+        //g =!true   //false
+//equal
+        String var1 = "g";
+        String var2 = "c";
+        System.out.print(var1.equals(var2));
+
+//massive
+        int [] mas = {10, 0, 0, 21, -5, 38};
+
+        int h1 = mas[0];
+        int h2 = mas[1];
+        int h3 = mas[2];
+        int h4 = mas[3];
+        int h5 = mas[4];
+        int h6 = mas[5];
+        System.out.println(" " + h1 + "," + h2 + h3 +  h4  + h5 + h6 + ",");
+
+        //System.out.println(args[0]);
+
+
+        String[] mas1 = {"a", "b", "c"};
+        String[] mas2 = {"a", "d", "c"};
+        String m1 = mas1[0];
+        String m2 = mas1[1];
+        String m3 = mas1[2];
+        String m4 = mas2[0];
+        String m5 = mas2[1];
+        String m6 = mas2[2];
+        System.out.println(m1 + m2 + m3 + m4);
+
+    }
+}
